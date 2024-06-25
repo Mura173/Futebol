a74c29d975c7d1cc0705ef380c43bc15e5e183a1
Murilo Ângelo <murapb07@gmail.com>
2024-06-25 10:33:46 -0300

    store manager upd

---
+++ Futebol/Assets/Scripts/LojaScript/Bolas.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolas
+{
+    public int bolasID;
+    public string nomeSprite;
+    public int bolasPreco;
+    public bool comprou;
+}
